86b6649c1f2196c86647c1524d5e529d77c545e0
malwazan <alwazan1234@gmail.com>
2023-05-27 23:36:39 +0500

    fix: npm install

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Book-IT'),
+      dir: require('path').join(__dirname, './coverage/nanoreads'),
       subdir: '.',
       reporters: [
         { type: 'html' },
